ea657f01e3bef9aea678d569703138425e2fa001
Grzegorz Marszałek <grzegorzmarszalek92@gmail.com>
2020-06-14 22:33:33 +0200

    Add Sass and MATERIAL-UI

--- src/components/VideoList/VideoList.js
+++ src/components/VideoList/VideoList.js
@@ -1,17 +1,19 @@
 import React from "react";
 
+import Paper from "@material-ui/core/Paper";
+
+import "./styles.scss";
+
 function VideoList(props) {
   return (
     <div className="video-list">
       {props.videos.map((video, index) => (
-        <div key={`viedo-${index}`}>
-          <img src={video.video_thumbnail} alt="viedo-thumbnail"></img>
-          <h3>
-            <a href="#" onClick={(event) => props.handleClick(event, video)}>
-              {video.title}
-            </a>
-          </h3>
-        </div>
+        <Paper className="video-list--item" key={`viedo-${index}`} elevation={3}>
+          <a href="#" onClick={(event) => props.handleClick(event, video)}>
+            <img src={video.video_thumbnail} alt="viedo-thumbnail"></img>
+          </a>
+          <p className="video-list--title">{video.title}</p>
+        </Paper>
       ))}
     </div>
   );
