ed6759c0618b54a2efc490ab375e5c12afe11ae3
Grzegorz Marszałek <grzegorzmarszalek92@gmail.com>
2020-06-22 22:25:22 +0200

    add API library

--- src/components/DisplayVideoList/DisplayVideoList.js
+++ src/components/DisplayVideoList/DisplayVideoList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 
+import api from "../../api";
 import { Link } from "react-router-dom";
 
 function DisplayVideoList() {
@@ -10,15 +11,16 @@ function DisplayVideoList() {
   }, []);
 
   const fetchVideos = async () => {
+    // const data = await api.get("/movies");
     const data = await fetch("/database.json");
     const videos = await data.json();
-    setVideos(videos);
+    setVideos(data);
   };
 
   return (
     <div>
       {videos.map((video) => (
-        <h3>
+        <h3 key={`video-${video.id}`}>
           <Link to={`/video/${video.id}`}>{video.title}</Link>
         </h3>
       ))}
